822afd2f95898110c725802a169f624132ce48df
sidhantss <sidhantsharmavlog@gmail.com>
2022-10-04 16:25:52 -0400

    updates in server.js

--- server.js
+++ server.js
@@ -7,8 +7,7 @@ console.log('Express http server listening on port http://localhost:' + HTTP_POR
 
 app.get("/", (req, res) => {
     res.send("<body style='background: white;'><h2>Declaration (Instruction: text size in heading 2):</h2> <p>Instruction: The rest text is displayed in paragraph as shown in screenshot.</p>" +
-    "<p> I declare that this test is my own work in accordance with Seneca Academic Policy. No part of this test has been copied manually or electronically"+
-    "<p>from any other source.</p>"+
+    "<p> I declare that this test is my own work in accordance with Seneca Academic Policy. No part of this test has been copied manually or electronically from any other source."+
     "<p>Name: <mark><b>Sidhant Sharma </b></mark></p>"+
     "Student Number:<mark>123151219</mark></p>"+
     "<p><a href='/test1'>Go to Test1</a></p></body>" + "Instruction: Test1 is a link, which directs users to the route test1. ")
